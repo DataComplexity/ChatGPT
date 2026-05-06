4c856e8d4c828c7c605289d8e47ed4ab96b642f3
DataComplexity <lakshyasaini.work12345@gmail.com>
2026-05-06 21:58:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,10 +35,15 @@ export default function App() {
     }
   }, [text]);
 
+  const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : "https://chatgpt-backend-nry5.onrender.com";
+
   // Function to handle sending data to the API
   const sendToAPI = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/chat", {
+      const response = await fetch(`${API_URL}/api/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: text }),
